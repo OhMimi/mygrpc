ed03370a4ed23efc1ca062306b39ff705fe06ec2
hulk_hsu <hulk_hsu@mail.chungyo.net>
2020-10-08 10:06:21 +0800

    [update]add getUserInfo

--- client/client.go
+++ client/client.go
@@ -11,12 +11,20 @@ import (
 
 func main() {
 
+	// 透過Dial()負責跟gRPC服務端建立起連線
 	conn, err := grpc.Dial("localhost:9999", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("連線失敗 : %v", err)
 	}
 	defer conn.Close()
 
+	EchoReq(conn)
+
+	UserReq(conn)
+}
+
+func EchoReq(conn *grpc.ClientConn) {
+	// 注入連線, 返回NewEchoServerClient對象
 	c := pb.NewEchoServerClient(conn)
 
 	r, err := c.Echo(context.Background(), &pb.EchoRequest{
@@ -27,4 +35,18 @@ func main() {
 	}
 
 	log.Printf("回傳結果：%s , 時間:%d", r.Message, r.Unixtime)
+}
+
+func UserReq(conn *grpc.ClientConn) {
+	// 注入連線, 返回NewEchoServerClient對象
+	c := pb.NewUserServiceClient(conn)
+
+	r, err := c.GetUserInfo(context.Background(), &pb.UserRequest{
+		ID: 1,
+	})
+	if err != nil {
+		log.Fatalf("無法執行 Plus 函式：%v", err)
+	}
+
+	log.Printf("回傳使用者名稱：%s , 年紀:%d", r.Name, r.Age)
 }
--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	pb "mygrpc/echospec"
 	"net"
@@ -11,9 +12,21 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
-type EchoServer struct{}
+// 準備幾個fake users
+var users = map[int32]pb.UserResponse{
+	1: {
+		Name: "HuanNa",
+		Age:  20,
+	},
+	2: {
+		Name: "Kevin",
+		Age:  30,
+	},
+}
 
-func (e *EchoServer) Echo(c context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
+type Server struct{}
+
+func (s *Server) Echo(c context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	log.Printf("receive client request, client send:%s\n", req.Message)
 	return &pb.EchoResponse{
 		Message:  req.Message,
@@ -21,8 +34,19 @@ func (e *EchoServer) Echo(c context.Context, req *pb.EchoRequest) (*pb.EchoRespo
 	}, nil
 }
 
+func (s *Server) GetUserInfo(c context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
+	// 查找map有沒有該user, 有就回覆, 否則就回錯誤
+	if user, ok := users[req.GetID()]; ok {
+		return &user, nil
+	}
+
+	log.Printf("req : %v\n", req)
+	return nil, errors.New("user not found")
+}
+
 func main() {
 
+	// 註冊端口來提供gRPC服務
 	apiListener, err := net.Listen("tcp", ":9999")
 	if err != nil {
 		log.Fatal(err)
@@ -30,13 +54,18 @@ func main() {
 	}
 
 	// 註冊grpc
-	es := &EchoServer{}
+	es := &Server{}
 
+	// 建構一個gRPC服務端實例
 	grpc := grpc.NewServer()
+
+	// 註冊服務
 	pb.RegisterEchoServerServer(grpc, es)
+	pb.RegisterUserServiceServer(grpc, es)
 
 	reflection.Register(grpc)
 
+	// grpc server服務啟用
 	if err := grpc.Serve(apiListener); err != nil {
 		log.Fatal(" grpc.Serve Error: ", err)
 		return
